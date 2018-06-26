6e5428ee3844f548d87dfd7b941df2d7a6b7ea23
Chris Clarkson <chris.clarkson@cjcsoftwaresolutions.co.uk>
2018-06-26 10:30:05 +0100

    Updated script to add header print message and check for all protocols in egress rule

--- security_groups.py
+++ security_groups.py
@@ -5,6 +5,16 @@ client = boto3.client('ec2')
 
 secgrps = client.describe_security_groups()
 
+def header_message():
+    print('AWS Security Group Analysis')
+    print('------------------------------------------------------------------------')
+    print('[+] Author: Chris Clarkson')
+    print('[+] Company: CJC Software Solutions Ltd.')
+    print('[+] License: MIT')
+    print('[+] Python Script to Analyse the Security Groups of an AWS Account')
+    print('[+] usage: security_groups.py')
+    print('------------------------------------------------------------------------')
+    print()
 
 def security_group_egress_check(groups):
     '''
@@ -16,12 +26,12 @@ def security_group_egress_check(groups):
     for grp in groups['SecurityGroups']:
         for egress in grp['IpPermissionsEgress']:
             for range in egress['IpRanges']:
-                if range['CidrIp'] == '0.0.0.0/0':
+                if  range['CidrIp'] == '0.0.0.0/0' and egress['IpProtocol'] == '-1':
                     print(
-                        "Egress rules for Security Group \"{}\", ID: {} are too open, consider restricting Egress rules to known IPs".format(
+                        "[+] Egress rules for Security Group \"{}\", ID: {} are too open, consider restricting Egress rules to known IPs".format(
                             grp['GroupName'], grp['GroupId']))
 
 
 
-
+header_message()
 security_group_egress_check(secgrps)
